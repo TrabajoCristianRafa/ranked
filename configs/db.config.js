6aa1fea2e0c6451a737e56cd3f70e4191ab01ea7
christiangar <32417561+christiangar@users.noreply.github.com>
2018-02-24 18:08:15 +0100

    Added db in processenv

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const DB_NAME = 'ranked';
-const MONGO_URI = `mongodb://localhost/${DB_NAME}`;
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URI)
